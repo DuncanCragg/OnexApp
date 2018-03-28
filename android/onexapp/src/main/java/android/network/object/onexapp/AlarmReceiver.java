2d9147f99b8e21548b2de683fcdabcebb68503f0
DuncanCragg <cyrus@cilux.org>
2018-03-28 09:05:32 +0100

    move showNotification

--- android/onexapp/src/main/java/android/network/object/onexapp/AlarmReceiver.java
+++ android/onexapp/src/main/java/android/network/object/onexapp/AlarmReceiver.java
@@ -1,24 +1,11 @@
-
 package network.object.onexapp;
 
 import android.content.*;
 import android.app.*;
 
 public class AlarmReceiver extends BroadcastReceiver {
-
     @Override
     public void onReceive(Context context, Intent intent) {
-        showNotification(context, "Onex", "Alarm");
-    }
-
-    public void showNotification(Context context, String title, String text){
-         PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, new Intent(context, OnexNativeActivity.class), PendingIntent.FLAG_UPDATE_CURRENT);
-         Notification.Builder notifbuilder = new Notification.Builder(context);
-         notifbuilder.setContentTitle(title)
-                     .setContentText(text)
-                     .setSmallIcon(R.drawable.icon)
-                     .setContentIntent(pendingIntent);
-         NotificationManager notifMgr = (NotificationManager)context.getSystemService(context.NOTIFICATION_SERVICE);
-         notifMgr.notify(12345, notifbuilder.build());
+        OnexNativeActivity.showNotification(context, "Onex", "Alarm");
     }
 }
--- android/onexapp/src/main/java/android/network/object/onexapp/OnexNativeActivity.java
+++ android/onexapp/src/main/java/android/network/object/onexapp/OnexNativeActivity.java
@@ -209,5 +209,16 @@ public class OnexNativeActivity extends NativeActivity implements KeyEvent.Callb
       AlarmManager am=(AlarmManager)getSystemService(Context.ALARM_SERVICE);
       am.set(AlarmManager.RTC_WAKEUP, when, pendingIntent);
     }
+
+    static public void showNotification(Context context, String title, String text){
+      PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, new Intent(context, OnexNativeActivity.class), PendingIntent.FLAG_UPDATE_CURRENT);
+      Notification.Builder notifbuilder = new Notification.Builder(context);
+      notifbuilder.setContentTitle(title)
+                  .setContentText(text)
+                  .setSmallIcon(R.drawable.icon)
+                  .setContentIntent(pendingIntent);
+      NotificationManager notifMgr = (NotificationManager)context.getSystemService(context.NOTIFICATION_SERVICE);
+      notifMgr.notify(12345, notifbuilder.build());
+    }
 }
 
