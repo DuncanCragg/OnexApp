3914a46aeecafe7fa361ee0c7ae6d53d9a4482e2
DuncanCragg <duncan@cilux.org>
2020-02-27 15:27:40 +0000

    build for embedded OnexApp

---
+++ src/OnexAppIoT.c
@@ -0,0 +1,86 @@
+
+#include <stdlib.h>
+#include <boards.h>
+#include <onex-kernel/gpio.h>
+#include <onex-kernel/serial.h>
+#include <onex-kernel/blenus.h>
+#include <onex-kernel/time.h>
+#include <onex-kernel/log.h>
+#include <onf.h>
+#include <onr.h>
+
+object* button;
+object* light;
+char* buttonuid;
+char* lightuid;
+
+void button_changed(int);
+bool evaluate_button_io(object* button, void* pressed);
+bool evaluate_light_io(object* light, void* d);
+
+void* x;
+#define WHERESTHEHEAP(s) x = malloc(1); log_write("heap after %s: %x\n", s, x);
+
+int main()
+{
+  log_init();
+  time_init();
+  serial_init(0,0);
+  blenus_init(0);
+  onex_init("");
+
+  gpio_mode_cb(BUTTON_1, INPUT_PULLUP, button_changed);
+  gpio_mode(LED1_G, OUTPUT);
+  gpio_mode(LED2_B, OUTPUT);
+
+  onex_set_evaluators("evaluate_button", evaluate_edit_rule, evaluate_button_io, 0);
+  onex_set_evaluators("evaluate_light",  evaluate_edit_rule, evaluate_light_logic, evaluate_light_io, 0);
+  onex_set_evaluators("evaluate_device", evaluate_device_logic, 0);
+
+  button=object_new(0, "evaluate_button", "editable button", 4);
+  light =object_new(0, "evaluate_light",  "editable light", 4);
+  buttonuid=object_property(button, "UID");
+  lightuid=object_property(light, "UID");
+
+  object_property_set(button, "name", "£€§");
+
+  object_property_set(light, "light", "off");
+
+  object_set_evaluator(onex_device_object, (char*)"evaluate_device");
+  object_property_add(onex_device_object, (char*)"io", buttonuid);
+  object_property_add(onex_device_object, (char*)"io", lightuid);
+
+  onex_run_evaluators(lightuid, 0);
+
+  gpio_set(LED1_G, 0);
+  gpio_set(LED2_B, 1);
+
+  while(1){
+    onex_loop();
+  }
+}
+
+void button_changed(int pressed)
+{
+  onex_run_evaluators(buttonuid, (void*)(bool)pressed);
+}
+
+bool evaluate_button_io(object* button, void* pressed)
+{
+  char* s=(char*)(pressed? "down": "up");
+  object_property_set(button, "state", s);
+  return true;
+}
+
+bool evaluate_light_io(object* light, void* d)
+{
+  if(object_property_is(light, "light", "on")){
+    WHERESTHEHEAP("evaluate_light_io on");
+    gpio_set(LED2_B, 0);
+  } else {
+    WHERESTHEHEAP("evaluate_light_io off");
+    gpio_set(LED2_B, 1);
+  }
+  return true;
+}
+
