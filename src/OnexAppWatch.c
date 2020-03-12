d993d0ff74ae63730643ef9101d07dc6cdb19800
DuncanCragg <duncan@cilux.org>
2020-03-12 10:55:17 +0000

    use gfx lib

--- src/OnexAppWatch.c
+++ src/OnexAppWatch.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <boards.h>
 #if defined(BOARD_PINETIME)
-#include <nrf_gfx.h>
+#include <onex-kernel/gfx.h>
 #endif
 #include <onex-kernel/gpio.h>
 #if defined(HAS_SERIAL)
@@ -26,14 +26,6 @@ bool evaluate_light_io(object* light, void* d);
 void* x;
 #define WHERESTHEHEAP(s) x = malloc(1); log_write("heap after %s: %x\n", s, x);
 
-#if defined(BOARD_PINETIME)
-extern const nrf_lcd_t nrf_lcd_st7789;
-extern const nrf_gfx_font_desc_t orkney_24ptFontInfo;
-
-static const nrf_lcd_t * p_lcd = &nrf_lcd_st7789;
-static const nrf_gfx_font_desc_t * p_font = &orkney_24ptFontInfo;
-#endif
-
 int main()
 {
   log_init();
@@ -44,10 +36,12 @@ int main()
   blenus_init(0);
 
 #if defined(BOARD_PINETIME)
-  APP_ERROR_CHECK(nrf_gfx_init(p_lcd));
-  nrf_gfx_screen_fill(p_lcd, 0xC618);
-  nrf_gfx_point_t text_start = NRF_GFX_POINT(5,5);
-  APP_ERROR_CHECK(nrf_gfx_print_fast2(p_lcd, &text_start, 0x001F, "OnexOS", p_font, true));
+  gfx_init();
+  gfx_screen_colour(0xC618);
+  gfx_text_colour(0x001F);
+  gfx_screen_fill();
+  gfx_pos(30, 30);
+  gfx_text("OnexOS");
 #endif
 
   onex_init("");
