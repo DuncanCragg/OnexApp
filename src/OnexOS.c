ac7cd86d725c1cfe80da1e19bb3feb484f0465be
DuncanCragg <duncan@cilux.org>
2020-05-11 20:34:54 +0100

    dfu start using boot api

--- src/OnexOS.c
+++ src/OnexOS.c
@@ -3,8 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <boards.h>
-#include <nrf_pwr_mgmt.h>
-#include <nrf_bootloader_info.h>
+#include <onex-kernel/boot.h>
 #include <onex-kernel/log.h>
 #include <onex-kernel/time.h>
 #include <onex-kernel/gpio.h>
@@ -245,13 +244,10 @@ bool evaluate_touch_io(object* o, void* d)
 bool evaluate_button_io(object* o, void* d)
 {
   object_property_set(button, "state", button_pressed? "down": "up");
+
   // FOR TESTING
-  if(button_pressed){
-    sd_power_gpregret_clr(0, 0xffffffff);
-    sd_power_gpregret_set(0, BOOTLOADER_DFU_START);
-    nrf_pwr_mgmt_shutdown(NRF_PWR_MGMT_SHUTDOWN_GOTO_DFU);
-  }
-  // FOR TESTING
+  if(button_pressed) boot_dfu_start();
+
   return true;
 }
 
