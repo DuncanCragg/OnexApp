5e826caa33fb50a762226e093e0617d1c57fc2fe
DuncanCragg <duncan@cilux.org>
2020-07-03 17:14:16 +0100

    keep display on long enough to see reset after 5s

--- src/OnexOS.c
+++ src/OnexOS.c
@@ -239,7 +239,7 @@ int main()
 
   object_property_set(backlight, "light", "on");
   object_property_set(backlight, "level", "high");
-  object_property_set(backlight, "timeout", "5000");
+  object_property_set(backlight, "timeout", "6000");
   object_property_set(backlight, "touch", touchuid);
   object_property_set(backlight, "motion", motionuid);
   object_property_set(backlight, "button", buttonuid);
